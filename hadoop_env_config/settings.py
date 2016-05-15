d87997cd4e462b0ffbcf8e9929dfaa3a1f391cb6
ownport <ownport@gmail.com>
2016-05-15 20:58:29 +0300

    [+] single place for main settings

---
+++ hadoop_env_config/settings.py
@@ -0,0 +1,60 @@
+#
+#   Hadoop config files
+#
+CONFIG_FILES = {
+    'hdfs-site.xml': ['hadoop/conf/hdfs-site.xml', 'hadoop-conf/hdfs-site.xml'],
+    'core-site.xml': ['hadoop/conf/core-site.xml', 'hadoop-conf/core-site.xml'],
+    'hbase-site.xml': ['hbase/conf/hbase-site.xml', 'hbase-conf/hbase-site.xml'],
+    'hive-site.xml': ['hive/conf/hive-site.xml', 'hive-conf/hive-site.xml'],
+    'yarn-site.xml': ['hadoop/conf/yarn-site.xml', 'hadoop-conf/yarn-site.xml'],
+    'mapred-site.xml': ['hadoop/conf/mapred-site.xml', 'hadoop-conf/mapred-site.xml'],
+    'oozie-site.xml': ['oozie/conf/oozie-site.xml','oozie-conf/oozie-site.xml' ],
+}
+
+#
+#   Parameters mapping for CDH4.x platforms
+#
+MAPPING_CDH4X = {
+    "core-site": {
+        "fs.defaultFS": "nameNode",
+        "hadoop.security.authentication": "securityAuthentication"
+    },
+    "mapred-site": {
+        "mapred.job.tracker": "jobTracker"
+    },
+    "hbase-site": {
+        "hbase.zookeeper.quorum": "hbaseZookeeperQuorum"
+    },
+    "hive-site": {
+        "hive.zookeeper.quorum": "hiveZookeeperQuorum",
+        "hive.metastore.uris": "hiveMetastoreUris",
+        "hive.metastore.kerberos.principal": "hiveMetastorePrincipal"
+    },
+    "oozie-site": {
+        "oozie.base.url": "oozieServerUrl"
+    }
+}
+
+#
+#   Parameters mapping for HDP2.2 platform
+#
+MAPPING_HDP2X = {
+    "core-site": {
+        "fs.defaultFS": "nameNode",
+        "hadoop.security.authentication": "securityAuthentication"
+    },
+    "yarn-site": {
+        "yarn.resourcemanager.address": "jobTracker"
+    },
+    "hbase-site": {
+        "hbase.zookeeper.quorum": "hbaseZookeeperQuorum"
+    },
+    "hive-site": {
+        "hive.zookeeper.quorum": "hiveZookeeperQuorum",
+        "hive.metastore.uris": "hiveMetastoreUris",
+        "hive.metastore.kerberos.principal": "hiveMetastorePrincipal"
+    },
+    "oozie-site": {
+        "oozie.base.url": "oozieServerUrl"
+    }
+}
