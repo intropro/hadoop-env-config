ce3d38830dec6ce5eacc9a1f4706a5574a20d0b4
ownport <ownport@gmail.com>
2016-06-24 08:54:31 +0300

    [+] setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hadoop-env-config',
+    version='0.2.2',
+    py_modules=['hadoop_env_config'],
+    # entry_points='''
+    #     [console_scripts]
+    #     hadoop-env-config=hadoop-env-config.main:run
+    # ''',
+)
