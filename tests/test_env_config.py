7be2f3720d02130af033870c1ff25be26488fd34
ownport <ownport@gmail.com>
2016-07-02 14:58:03 +0300

    [+] test_hadoop_env_config()

--- tests/test_env_config.py
+++ tests/test_env_config.py
@@ -11,6 +11,10 @@ def get_settings_path():
 
     return os.path.join(get_abspath_test_path(), 'resources/settings/')
 
+def get_configs_path():
+
+    return os.path.join(get_abspath_test_path(), 'resources/conf-dirs/')
+
 
 @pytest.fixture(params=['hdp-with-server.settings', 'hdp-wo-server.settings'])
 def hdp_settings_path(request):
@@ -41,6 +45,17 @@ def test_env_config_incorrect_path_to_settings():
         conf = env_config.HadoopEnvConfig(os.path.join(get_settings_path(), 'shadow.settings'))
 
 
-def test_hadoop_env_config_init(hdp_settings_path):
+def test_hadoop_env_config(hdp_settings_path):
 
     conf = env_config.HadoopEnvConfig(hdp_settings_path)
+    conf.settings['config-dir'] = os.path.join(get_configs_path(), 'hdp-2.2.8/')
+    env_props = conf.parse()
+
+    assert isinstance(env_props, dict) and len(env_props) != 0
+    assert 'oozieServer' in env_props and env_props['oozieServer'] == 'http://oozie.bigdata.hosts:11000'
+    assert 'jobTracker' in env_props and env_props['jobTracker'] == 'yarn.bigdata.hosts:8050'
+    assert 'nameNode' in env_props and env_props['nameNode'] == 'hdfs://nn.bigdata.hosts:8020'
+
+    assert 'sshUserName' in env_props and env_props['sshUserName'] == 'username'
+    assert 'sshPassword' in env_props and env_props['sshPassword'] == 'password'
+    
