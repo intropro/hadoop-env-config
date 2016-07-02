9bb4b81f73663c8cecb997dab99994e3aa4ee7a0
ownport <ownport@gmail.com>
2016-07-02 11:42:47 +0300

    [+] settings_path fixture

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,5 +1,9 @@
+import os
+import pytest
+
 from hadoop_env_config.common import create_env_props
 
-# def test_create_env_props():
-#
-#     assert create_env_props()
+@pytest.fixture(params=['cdh-with-server.settings', 'cdh-wo-server.settings'])
+def settings_path(request):
+    path = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(path, 'resources/settings/', request.param)
