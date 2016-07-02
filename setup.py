e88b79735a456bd50d9b78a9d0059d4b094c9aff
ownport <ownport@gmail.com>
2016-07-02 11:18:33 +0300

    [*] move to main.run()

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='hadoop-env-config',
     version='0.2.2',
     py_modules=['hadoop_env_config'],
-    # entry_points='''
-    #     [console_scripts]
-    #     hadoop-env-config=hadoop-env-config.main:run
-    # ''',
+    entry_points='''
+        [console_scripts]
+        hadoop-env-config=hadoop_env_config.main:run
+    ''',
 )
